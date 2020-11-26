affb5940b48bfbfed8c0c5b11285498ab0edccad
thawatchai <thawatchai.sea2@gmail.com>
2020-11-26 10:27:07 +0700

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,7 +4,7 @@ import * as moment from 'moment-timezone';
 import * as mqttClient from '../vendor/mqtt';
 import { MqttClient } from 'mqtt';
 import { CountdownComponent } from 'ngx-countdown';
-
+import * as XLSX from 'xlsx';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -157,6 +157,21 @@ export class AppComponent implements OnInit {
         }
       });
     });
+  }
+
+
+  exportexcel(): void {
+    /* table id is passed over here */
+    let element = document.getElementById('excel-table');
+    const ws: XLSX.WorkSheet = XLSX.utils.table_to_sheet(element);
+
+    /* generate workbook and add the worksheet */
+    const wb: XLSX.WorkBook = XLSX.utils.book_new();
+    XLSX.utils.book_append_sheet(wb, ws, 'Sheet1');
+
+    /* save to file */
+    XLSX.writeFile(wb, 'timeofficial-export.xlsx');
+
   }
 
 }
